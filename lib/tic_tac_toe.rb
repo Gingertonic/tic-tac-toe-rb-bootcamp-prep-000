e900aa5b8e1360aec73ffb7e2c7b547fcc22db5e
Beth Schofield <bethmschofield@gmail.com>
2018-02-04 05:28:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,15 +45,15 @@ def turn(board)
   end
 end
 
-def turn_count(board)
-  count = 0
-  board.each do |i|
-    if i == "X" || i == "O"
-      count += 1
-    end
-    return count
-  end
-end
+# def turn_count(board)
+#   count = 0
+#   board.each do |i|
+#     if i == "X" || i == "O"
+#       count += 1
+#     end
+#     return count
+#   end
+# end
 
 # def current_player(board)
 #   turn_count % 2 == 0 ? "X" : "O"
